3e3cce91ce2040ae4174c36096eb342303fd372c
lialess1808 <nathaliacamposlessa1@gmail.com>
2024-10-30 01:07:40 -0300

    update server settings

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const port = 3000;
 const app = express();
 
-app.use(express.static("public"));
+// Configurações do Express
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
+// Chave privada para o JWT
+const privateKey = "conectamaes.linceonline.com.br.200420052005.web_token";
+
+// Configuração do banco de dados
 const db = {
     host: '54.173.126.116',
     port: 3306,
@@ -16,6 +22,20 @@ const db = {
     database: 'tet-conectamaes'
 };
 
+// Middleware para verificar JWT
+const middlewareVerifyJWT = (req, res, next) => {
+    const jwtToken = req.headers["authorization"];
+    jwt.verify(jwtToken, privateKey, (err, userInfo) => {
+        if (err) {
+            res.status(403).end();
+            return;
+        }
+        req.userInfo = userInfo;
+        next();
+    });
+};
+
+// Função para executar consultas SQL
 const execSQLQuery = (sqlQry, params, res) => {
     const connection = mysql.createConnection(db);
     connection.query(sqlQry, params, (error, results) => {
@@ -29,6 +49,7 @@ const execSQLQuery = (sqlQry, params, res) => {
     });
 };
 
+// Função assíncrona para consulta SQL
 async function resultSQLQuery(sqlQry, id) {
     const connection = await mysql.createConnection(db);
     let [result] = await connection.promise().query(sqlQry, id);
@@ -40,17 +61,29 @@ async function resultSQLQuery(sqlQry, id) {
     }
 }
 
+// Rotas
 app.get('/', (req, res) => {
     res.send('API está funcionando!');
 });
 
 // Buscar todos os usuários
-app.get('/showUsers', (req, res) => {
+app.get('/showUsers', middlewareVerifyJWT, (req, res) => {
     execSQLQuery("SELECT * FROM Usuario", [], res);
 });
 
+// Buscar um usuário específico
+app.get('/showUser/:id', middlewareVerifyJWT, (req, res) => {
+    execSQLQuery('SELECT * FROM Usuario WHERE idUsuario =?', [req.params.id], res);
+});
+
+// Endpoint para buscar uma propriedade do usuário
+app.get('/getUserProperty/:id/:property', middlewareVerifyJWT, (req, res) => {
+    const data = [req.params.id];
+    execSQLQuery('SELECT ' + req.params.property + ' FROM Usuario WHERE idUsuario =?', data, res);
+});
+
 // Inserir um novo usuário
-app.post('/insertUser ', (req, res) => {
+app.post('/insertUser', (req, res) => {
     const { nomeCompleto, nomeDeUsuario, email, senha, telefone, estado, dataNascimentoUsuario, tema, isAdmin } = req.body;
     const data = [nomeCompleto, nomeDeUsuario, email, senha, telefone, estado, dataNascimentoUsuario, tema, isAdmin];
     execSQLQuery("INSERT INTO Usuario (nomeCompleto, nomeDeUsuario, email, senha, telefone, estado, dataNascimentoUsuario, tema, isAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", data, res);
@@ -60,21 +93,44 @@ app.post('/insertUser ', (req, res) => {
 app.post('/login', async (req, res) => {
     const id = [req.body.email, req.body.senha];
     let [result] = await resultSQLQuery('SELECT * FROM Usuario WHERE email=? and senha=?', id);
-    console.log(result)
+    console.log(result);
+
     if (result) {
-        res.json({
-            "mensagem": "",
-            "id": result.idUsuario
+        jwt.sign(req.body, privateKey, (err, token) => {
+            if (err) {
+                res.status(500).json({ mensagem: "Erro ao gerar o JWT" });
+                return;
+            }
+
+            console.log(token);
+            res.set("x-access-token", token);
+            res.json({
+                mensagem: "",
+                id: result.idUsuario, // Adicionei o id aqui
+                "token": token
+            });
         });
     } else {
         res.json({
-            "mensagem": "Usuário inválido!"
+            mensagem: "Usuário inválido!"
         });
     }
 });
 
+// Editar um usuário
+app.put('/editUser/:id', middlewareVerifyJWT, (req, res) => {
+    const { nomeCompleto, email, senha, telefone, biografia, estado, dataNascimentoUsuario, tema, isAdmin, linkFotoPerfil, chavePix } = req.body;
+    const data = [nomeCompleto, email, senha, telefone, biografia, estado, dataNascimentoUsuario, tema, isAdmin, linkFotoPerfil, chavePix, req.params.id];
+    execSQLQuery('UPDATE Usuario SET nomeCompleto =?, email =?, senha =?, telefone =?, biografia =?, estado = ?, dataNascimentoUsuario =?, tema =?, isAdmin =?, linkFotoPerfil =?, chavePix =? WHERE idUsuario =?', data, res);
+});
+
+// Deletar um usuário específico
+app.delete('/deleteUser/:id', middlewareVerifyJWT, (req, res) => {
+    execSQLQuery('DELETE FROM Usuario WHERE idUsuario = ?', [req.params.id], res);
+});
+
 // Endpoint para criar uma nova publicação
-app.post('/posts', (req, res) => {
+app.post('/posts', middlewareVerifyJWT, (req, res) => {
     const { conteudoEnvio, tituloEnvio, isConcluidoEnvio, currentUserId, postType } = req.body;
 
     if (!conteudoEnvio) {
@@ -94,8 +150,13 @@ app.post('/posts', (req, res) => {
     execSQLQuery(insertNewPost, data, res);
 });
 
+// Buscar todas as publicações
+app.get('/showPosts', middlewareVerifyJWT, (req, res) => {
+    execSQLQuery("SELECT * FROM Publicacao", [], res);
+});
+
 // Endpoint para buscar publicações
-app.get('/posts', (req, res) => {
+app.get('/posts', middlewareVerifyJWT, (req, res) => {
     const postType = req.query.postType || '';
     const whereClause = postType === '' ? "p.tipoPublicacao <> 'Auxilio'" : `p.tipoPublicacao = ?`;
 
@@ -124,22 +185,25 @@ app.get('/posts', (req, res) => {
     execSQLQuery(baseQuery, data, res);
 });
 
-// Endpoint para deletar uma publicação
-app.delete('/posts/:id', (req, res) => {
-    const id = req.params.id;
+// Buscar uma publicação específica
+app.get('/showPost/:id', middlewareVerifyJWT, (req, res) => {
+    execSQLQuery('SELECT * FROM Publicacao WHERE idPublicacao = ?', [req.params.id], res);
+});
 
-    if (!id) {
-        return res.status(400).send('ID da publicação não pode estar vazio.');
-    }
+// Editar uma publicação
+app.put('/editPost/:id', middlewareVerifyJWT, (req, res) => {
+    const { tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido } = req.body;
+    const data = [tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido, req.params.id];
+    execSQLQuery('UPDATE Publicacao SET tipoPublicacao = ?, conteudo = ?, linkAnexo = ?, titulo = ?, isAnonima = ?, isConcluido = ? WHERE idPublicacao = ?', data, res);
+});
 
-    const dQuery = `DELETE FROM Publicacao WHERE idPublicacao = ?`;
-    const data = [id];
-
-    execSQLQuery(dQuery, data, res);
+// Deletar uma publicação específica
+app.delete('/deletePost/:id', middlewareVerifyJWT, (req, res) => {
+    execSQLQuery('DELETE FROM Publicacao WHERE idPublicacao = ?', [req.params.id], res);
 });
 
 // Endpoint para lidar com likes
-app.post('/posts/:id/like', (req, res) => {
+app.post('/posts/:id/like', middlewareVerifyJWT, (req, res) => {
     const idPost = req.params.id;
     const idUser = req.body.idUser;
 
@@ -154,80 +218,16 @@ app.post('/posts/:id/like', (req, res) => {
         if (!results.length) {
             // Inserir like
             const insertLike = `INSERT INTO curtirPublicacao (idPublicacao, idUsuario) VALUES (?, ?)`;
-            const data = [idPost, idUser];
-
-            execSQLQuery(insertLike, data, (err, result) => {
-                if (err) {
-                    return res.status(500).send(`Erro ao registrar curtida: ${err.message}`);
-                }
-                res.send('Curtida registrada com sucesso!');
-            });
+            execSQLQuery(insertLike, [idPost, idUser], res);
         } else {
             // Remover like
             const deleteLike = `DELETE FROM curtirPublicacao WHERE idPublicacao = ? AND idUsuario = ?`;
-            const data = [idPost, idUser];
-
-            execSQLQuery(deleteLike, data, (err, result) => {
-                if (err) {
-                    return res.status(500).send(`Erro ao remover curtida: ${err.message}`);
-                }
-                res.send('Curtida removida com sucesso!');
-            });
+            execSQLQuery(deleteLike, data, res);
         }
     });
 });
 
-// Buscar um usuário específico
-app.get('/showUser/:id', (req, res) => {
-    execSQLQuery('SELECT * FROM Usuario WHERE idUsuario =?', [req.params.id], res);
-});
-
-app.get('/getUserProperty/:id/:property', (req, res) => {
-    const data = [req.params.id];
-    execSQLQuery('SELECT ' + req.params.property + ' FROM Usuario WHERE idUsuario =?', data, res);
-});
-
-// Deletar um usuário específico
-app.delete('/deleteUser/:id', (req, res) => {
-    execSQLQuery('DELETE FROM Usuario WHERE idUsuario = ?', [req.params.id], res);
-});
-
-// Editar um usuário
-app.put('/editUser/:id', (req, res) => {
-    const { nomeCompleto, email, senha, telefone, biografia, estado, dataNascimentoUsuario, tema, isAdmin, linkFotoPerfil, chavePix } = req.body;
-    const data = [nomeCompleto, email, senha, telefone, biografia, estado, dataNascimentoUsuario, tema, isAdmin, linkFotoPerfil, chavePix, req.params.id];
-    execSQLQuery('UPDATE Usuario SET nomeCompleto =?, email =?, senha =?, telefone =?, biografia =?, estado = ?, dataNascimentoUsuario =?, tema =?, isAdmin =?, linkFotoPerfil =?, chavePix =? WHERE idUsuario =?', data, res);
-});
-
-// Inserir uma nova publicação
-app.post('/insertPost', (req, res) => {
-    const { tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido, idUsuario } = req.body;
-    const data = [tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido, idUsuario];
-    execSQLQuery("INSERT INTO Publicacao (tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido, idUsuario) VALUES (?, ?, ?, ?, ?, ?, ?)", data, res);
-});
-
-// Buscar todas as publicações
-app.get('/showPosts', (req, res) => {
-    execSQLQuery("SELECT * FROM Publicacao", [], res);
-});
-
-// Buscar uma publicação específica
-app.get('/showPost/:id', (req, res) => {
-    execSQLQuery('SELECT * FROM Publicacao WHERE idPublicacao = ?', [req.params.id], res);
-});
-
-// Deletar uma publicação específica
-app.delete('/deletePost/:id', (req, res) => {
-    execSQLQuery('DELETE FROM Publicacao WHERE idPublicacao = ?', [req.params.id], res);
-});
-
-// Editar uma publicação
-app.put('/editPost/:id', (req, res) => {
-    const { tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido } = req.body;
-    const data = [tipoPublicacao, conteudo, linkAnexo, titulo, isAnonima, isConcluido, req.params.id];
-    execSQLQuery('UPDATE Publicacao SET tipoPublicacao = ?, conteudo = ?, linkAnexo = ?, titulo = ?, isAnonima = ?, isConcluido = ? WHERE idPublicacao = ?', data, res);
-});
-
+// Inicializa o servidor
 app.listen(port, () => {
-    console.log(`API rodando na porta ${port}`);
+    console.log(`API rodando em http://localhost:${port}`);
 });
